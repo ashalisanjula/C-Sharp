14c302fd9b119736f9bce76ff6010ab1b4c9d5a6
Ashali Samaraweera <ashalisanjula@gmail.com>
2025-01-30 10:27:27 +0530

    Changing the folder structure

---
+++ Debug/Testing1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program{
+    public static double DivideNumbers(double numerator, double denominator){
+
+        if (denominator==0){
+            Console.WriteLine("Error: Division by zero is not allowed.");
+            return double.NaN; 
+        }
+        double result = numerator/denominator;
+        return result;
+    }
+
+    public static void Main(){
+        double result = DivideNumbers(10,0);
+        Console.WriteLine("The result is : " +result);
+    }
+}

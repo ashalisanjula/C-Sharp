c27d3e5713c8a1c820a418f72968680d185c38a5
Ashali Samaraweera <ashalisanjula@gmail.com>
2025-01-31 20:43:38 +0530

    Trying Lists in Polimorphism

---
+++ Inheritance & Polimorphism/Polimorphism with Lists/Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿
+class Animal{
+    public virtual void Makesound(){
+        Console.WriteLine("Animals makes sounds..");
+    }
+}
+
+class Dog : Animal{
+    public override void Makesound()
+    {
+        Console.WriteLine("Dog is Barking");
+    }
+}
+
+class Cat : Animal{
+    public override void Makesound()
+    {
+        Console.WriteLine("Cat is Meowing");
+    }
+}
+
+class Program{
+    public static void Main(){
+        List<Animal> animals = new List<Animal>{
+            new Dog(),
+            new Cat(),
+            new Dog(),
+            new Cat()
+        };
+
+        foreach (Animal animal in animals){
+            animal.Makesound();
+        }
+    }
+}

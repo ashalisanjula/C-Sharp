f1db08e6c1ab1804654884a39ee37cc73fc8ff58
Ashali Samaraweera <ashalisanjula@gmail.com>
2025-01-31 20:22:07 +0530

    Trying Inheritance

---
+++ Inheritance & Polimorphism/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Animal{
+    public virtual void MakeSound(){
+        Console.WriteLine("Animals makes Sound.");
+    }
+}
+
+public class Dog : Animal{
+    public override void MakeSound(){
+        Console.WriteLine("Dog is Barking.");
+    }
+}
+
+public class Cat : Animal{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat is Meowing.");
+    }
+}
+
+public class Program{
+    public static void Main(){
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        animal1.MakeSound();
+        animal2.MakeSound();
+    }
+}
+
+

cbb67b9682d7c55101612cd9983de6a3225d3a12
Ashali Samaraweera <ashalisanjula@gmail.com>
2025-01-31 20:32:51 +0530

    Trying Inheritance

---
+++ Inheritance & Polimorphism/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿interface IAnimal{
+    void Eat();
+     
+}
+
+class Dog : IAnimal{
+    public void Eat(){
+        Console.WriteLine("Dog is eating..");
+    }
+}
+
+class Cat : IAnimal{
+    public void Eat(){
+        Console.WriteLine("Cat is eating..");
+    }
+}
+
+class Program{
+    static void Main(){
+        IAnimal animal1 = new Dog();
+        IAnimal animal2 = new Cat();
+
+        animal1.Eat();
+        animal2.Eat();
+    }
+}
